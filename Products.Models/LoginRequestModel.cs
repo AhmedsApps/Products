3b2d66aa5a0cba5ebbc8f1c76c62ee317964481e
Ahmed Ali <ahmed_oso@hotmail.com>
2026-04-10 01:41:33 +0200

    intial controller and models project

---
+++ Products.Models/LoginRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Products.Models
+{
+    public class LoginRequestModel
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ Products.Server/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿namespace Products.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("CheckHealth")]
+        public IActionResult CheckHealth() => Ok("Healthy");
+    }
+}
